1dddba93d05226ae9e8d7f5c7acbee4c65364d4e
Isabelle BLASQUEZ <isabelle.blasquez31@gmail.com>
2022-04-28 16:39:17 +0200

    ajout BoissonTest

---
+++ src/test/java/cafetoeil/model/BoissonTest.java
@@ -0,0 +1,14 @@
+package cafetoeil.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoissonTest {
+
+	@Test
+	void test() {
+		assertTrue(true); //TODO
+	}
+
+}
